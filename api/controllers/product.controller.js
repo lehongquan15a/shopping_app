345e7a370668f9634fc3547e2dfce45f5ada0b1b
lehongquan15a <lehongquan15a@gmail.com>
2020-09-17 10:33:45 +0700

    Add  api products

--- api/controllers/product.controller.js
+++ api/controllers/product.controller.js
@@ -1,6 +1,6 @@
 const Product =  require('../../model/product.model');
 
-module.exports.index = async (req, res, next) => {
+module.exports.index = async (req, res) => {
     const products = await Product.find();
     res.json(products)
 }
