6f526deb1cdf7c5bc34d8e5081f07edaae41fa9c
lehongquan15a <lehongquan15a@gmail.com>
2020-09-18 09:31:21 +0700

    add dotenv

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+require('dotenv').config();
 const express = require('express')
 const app = express()
 const port = 3000
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/shopping_app');
+mongoose.connect(process.env.MONGO_URL);
 
 
 app.use(express.json()) // for parsing application/json
